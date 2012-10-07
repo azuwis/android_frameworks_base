a3066cbaff4c7d12fe65f27d8c54b1e1fabb21e8
Eugene San (eugenesan) <eugenesan@gmail.com>
2012-10-07 09:40:28 +0200

    dandelion sounds
    Sound theme based on latest TouchWiz
    
    Replaces sounds:
    1. Keyboard
    2. Lock/Unlock
    3. Tick
    4. Dock
    5. Camera
    
    Adds:
    1. Select Ringtones
    2. Select Notifications
    
    Change-Id: I8d9706ad6d847df6c8cc8484aa91ec8070d898af

---
+++ data/sounds/AudioPackageDandelion.mk
@@ -0,0 +1,49 @@
+#
+# Stars Audio Package
+#
+# Include this file in a product makefile to include these audio files
+#
+#
+
+LOCAL_PATH:= frameworks/base/data/sounds
+
+# Notifications
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/notifications/ogg/Beep.ogg:system/media/audio/notifications/Beep.ogg \
+	$(LOCAL_PATH)/notifications/ogg/PureBell.ogg:system/media/audio/notifications/PureBell.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Bubbles.ogg:system/media/audio/notifications/Bubbles.ogg \
+	$(LOCAL_PATH)/notifications/ogg/CharmingBell.ogg:system/media/audio/notifications/CharmingBell.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Chirps.ogg:system/media/audio/notifications/Chirps.ogg \
+	$(LOCAL_PATH)/notifications/ogg/DewDrops.ogg:system/media/audio/notifications/DewDrops.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Flowers.ogg:system/media/audio/notifications/Flowers.ogg \
+	$(LOCAL_PATH)/notifications/ogg/GoodNews.ogg:system/media/audio/notifications/GoodNews.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Harmonics.ogg:system/media/audio/notifications/Harmonics.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Knock.ogg:system/media/audio/notifications/Knock.ogg \
+	$(LOCAL_PATH)/notifications/ogg/OnTime.ogg:system/media/audio/notifications/OnTime.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Opener.ogg:system/media/audio/notifications/Opener.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Postman.ogg:system/media/audio/notifications/Postman.ogg \
+	$(LOCAL_PATH)/notifications/ogg/TempleBell.ogg:system/media/audio/notifications/TempleBell.ogg \
+	$(LOCAL_PATH)/notifications/ogg/TicketyTock.ogg:system/media/audio/notifications/TicketyTock.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Whistle.ogg:system/media/audio/notifications/Wistle.ogg \
+
+# Ringtones
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ringtones/ogg/OverTheHorizon.ogg:system/media/audio/ringtones/OverTheHorizon.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/FogOnTheWater.ogg:system/media/audio/ringtones/FogOnTheWater.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/RaysOfTheSun.ogg:system/media/audio/ringtones/RaysOfTheSun.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/BlowingDandelionSeeds.ogg:system/media/audio/ringtones/BlowingDandelionSeeds.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/BugsStory.ogg:system/media/audio/ringtones/BugsStory.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/RainDrops.ogg:system/media/audio/ringtones/RainDrops.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/MoonlitBeach.ogg:system/media/audio/ringtones/MoonlitBeach.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/UnderwaterWorld.ogg:system/media/audio/ringtones/UnderwaterWorld.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/MidnightPicnic.ogg:system/media/audio/ringtones/MidnightPicnic.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/DriftingDownstream.ogg:system/media/audio/ringtones/DriftingDownstream.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/TheJourneyBegins.ogg:system/media/audio/ringtones/TheJourneyBegins.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/HighTide.ogg:system/media/audio/ringtones/HighTide.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/SeaBreeze.ogg:system/media/audio/ringtones/SeaBreeze.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/UndergroundPool.ogg:system/media/audio/ringtones/UndergroundPool.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/TheSecretForest.ogg:system/media/audio/ringtones/TheSecretForest.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/GentleSpringRain.ogg:system/media/audio/ringtones/GentleSpringRain.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/BirdsongByTheLake.ogg:system/media/audio/ringtones/BirdsongByTheLake.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/BeepBeep.ogg:system/media/audio/ringtones/BeepBeep.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/BeepOnce.ogg:system/media/audio/ringtones/BeepOnce.ogg \
