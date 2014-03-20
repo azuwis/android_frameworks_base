f46a8ce49a9ba07c2a6a0fd7c06c7905a67795bb
D4rKn3sSyS <jesdga95@gmail.com>
2014-03-20 21:06:55 +0700

    Base: Floating window with multitasking
    basic floating window are taken from AOSPA (HALO) by @D4rKn3sSyS
    
    Drawables courtesy of @Alex Cruz thanks to him :)
    
    with heavy change to get this work for multitasking
    
    this feature, has ability to:
    1. Moveable window to various position
    2. Scaling window with 2 finger like zoom in/out
    3. Double tap in actionbar or above window to fullscreen or back to floating window
    4. Automatically resize when Snapping into one of side (Top, Bottom, Left, Right)
       (taken from xhalofloatingwindow by @zst123)
    5. Automatically switch focus from other app (taken from multiwindow by @xplodwild)
    6. Unlimited floating window app (depends on RAM)
    
    this is still under testing, to make fully works
    so if there any bug because of this, DO NOT REPORT INTO OMNIROM JIRA
    
    to make app launch using Floating window feature
    
    you must add Intent.FLAG_FLOATING_WINDOW
    before start an activity
    
    for example you can look at https://gerrit.omnirom.org/6515
    
    need https://gerrit.omnirom.org/#/c/6561
    
    PS.17 : fix scaling
    PS.18 : fix layout calculation when rotate
    PS.19 : support minimize/maximize window (see App circle sidebar)
    PS.20 : fix previouse app
    
    Change-Id: Iad00c239de7d66e8bb45205ce626d4f08417b7cd
    
    Conflicts:
    	core/java/android/app/IActivityManager.java
    	core/java/android/content/Intent.java
    	core/res/res/values/custom_strings.xml

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -7425,6 +7425,24 @@ public final class ActivityManagerService extends ActivityManagerNative
         }
     }
 
+    public IBinder getActivityForTask(int task, boolean onlyRoot) {
+        final ActivityStack mainStack = mStackSupervisor.getFocusedStack();
+        synchronized(this) {
+            ArrayList<ActivityStack> stacks = mStackSupervisor.getStacks();
+            for (ActivityStack stack : stacks) {
+                TaskRecord r = stack.taskForIdLocked(task);
+
+                if (r != null && r.getTopActivity() != null) {
+                    return r.getTopActivity().appToken;
+                } else {
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+
     // =========================================================
     // THUMBNAILS
     // =========================================================
@@ -12618,6 +12636,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         app.foregroundActivities = false;
         app.hasShownUi = false;
         app.hasAboveClient = false;
+        app.hasClientActivities = false;
 
         mServices.killServicesLocked(app, allowRestart);
 
