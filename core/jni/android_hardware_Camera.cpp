Colt-Oreo/android_frameworks_base-1

FB: Add oneplus camera support
Change-Id: I8f247dd48a0eca610d2781b03c0fa84713adaf46
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/jni/android_hardware_Camera.cpp
@@ -43,6 +43,7 @@ enum {
 
 struct fields_t {
     jfieldID    context;
+    jfieldID    metadata_ptr;
     jfieldID    facing;
     jfieldID    orientation;
     jfieldID    canDisableShutterSound;
@@ -66,6 +67,7 @@ struct fields_t {
 
 static fields_t fields;
 static Mutex sLock;
+static CameraMetadata* mMeta_ptr;
 
 // provides persistent context for calls from native code to Java
 class JNICameraContext: public CameraListener
@@ -276,7 +278,12 @@ void JNICameraContext::copyAndPost(JNIEnv* env, const sp<IMemory>& dataPtr, int 
                         return;
                     }
                 }
-            } else {
+            } else if(msgType == 0x10000) {
+                camera_metadata_t * cMetaData = reinterpret_cast<camera_metadata_t*>(heapBase + offset);
+                *mMeta_ptr=(const camera_metadata_t*)cMetaData;
+                mMeta_ptr->sort();
+            }
+            else {
                 ALOGV("Allocating callback buffer");
                 obj = env->NewByteArray(size);
             }
@@ -896,6 +903,7 @@ static void android_hardware_Camera_takePicture(JNIEnv *env, jobject thiz, jint 
     JNICameraContext* context;
     sp<Camera> camera = get_native_camera(env, thiz, &context);
     if (camera == 0) return;
+    mMeta_ptr=reinterpret_cast<android::CameraMetadata*>(env->GetLongField(thiz,fields.metadata_ptr));
 
     /*
      * When CAMERA_MSG_RAW_IMAGE is requested, if the raw image callback
@@ -1221,6 +1229,7 @@ int register_android_hardware_Camera(JNIEnv *env)
 {
     field fields_to_find[] = {
         { "android/hardware/Camera", "mNativeContext",   "J", &fields.context },
+        { "android/hardware/Camera", "mMetadataPtr",   "J", &fields.metadata_ptr },
         { "android/hardware/Camera$CameraInfo", "facing",   "I", &fields.facing },
         { "android/hardware/Camera$CameraInfo", "orientation",   "I", &fields.orientation },
         { "android/hardware/Camera$CameraInfo", "canDisableShutterSound",   "Z",

azuwis/android_frameworks_base

Conflicts:
	core/java/android/hardware/camera2/CameraCharacteristics.java
Change-Id: If4b7e7fd590fe24a702ca406ec3645fdbeef469f

core/jni/android_hardware_Camera.cpp
@@ -43,6 +43,7 @@ enum {
 
 struct fields_t {
     jfieldID    context;
+    jfieldID    metadata_ptr;
     jfieldID    facing;
     jfieldID    orientation;
     jfieldID    canDisableShutterSound;
@@ -66,6 +67,7 @@ struct fields_t {
 
 static fields_t fields;
 static Mutex sLock;
+static CameraMetadata* mMeta_ptr;
 
 // provides persistent context for calls from native code to Java
 class JNICameraContext: public CameraListener
@@ -276,7 +278,12 @@ void JNICameraContext::copyAndPost(JNIEnv* env, const sp<IMemory>& dataPtr, int 
                         return;
                     }
                 }
-            } else {
+            } else if(msgType == 0x40000) {
+                camera_metadata_t * cMetaData = reinterpret_cast<camera_metadata_t*>(heapBase + offset);
+                *mMeta_ptr=(const camera_metadata_t*)cMetaData;
+                mMeta_ptr->sort();
+            }
+            else {
                 ALOGV("Allocating callback buffer");
                 obj = env->NewByteArray(size);
             }
@@ -911,6 +918,7 @@ static void android_hardware_Camera_takePicture(JNIEnv *env, jobject thiz, jint 
     JNICameraContext* context;
     sp<Camera> camera = get_native_camera(env, thiz, &context);
     if (camera == 0) return;
+    mMeta_ptr=reinterpret_cast<android::CameraMetadata*>(env->GetLongField(thiz,fields.metadata_ptr));
 
     /*
      * When CAMERA_MSG_RAW_IMAGE is requested, if the raw image callback
@@ -1239,6 +1247,7 @@ int register_android_hardware_Camera(JNIEnv *env)
 {
     field fields_to_find[] = {
         { "android/hardware/Camera", "mNativeContext",   "J", &fields.context },
+        { "android/hardware/Camera", "mMetadataPtr",   "J", &fields.metadata_ptr },
         { "android/hardware/Camera$CameraInfo", "facing",   "I", &fields.facing },
         { "android/hardware/Camera$CameraInfo", "orientation",   "I", &fields.orientation },
         { "android/hardware/Camera$CameraInfo", "canDisableShutterSound",   "Z",